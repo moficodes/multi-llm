d9054f91ad3017be933a649b4e40d5ea3b816ddc
Mofi Rahman <mofi@google.com>
2025-03-25 14:17:07 +0100

    add try catch to server action

--- app/actions.ts
+++ app/actions.ts
@@ -6,20 +6,22 @@ export async function modelInfo(): Promise<model[]> {
   const MODEL = process.env.LLM_URLS || "";
   const urls = MODEL.split(",");
   const models: model[] = [];
-
-  for (let i = 0; i < urls.length; i++) {
-    const url = urls[i];
-    const response = await fetch(`${url}/v1/models`, {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
-    const json = await response.json();
-
-    models.push({ name: json.data[0].id, url: url });
+  try {
+    for (let i = 0; i < urls.length; i++) {
+      const url = urls[i];
+      const response = await fetch(`${url}/v1/models`, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      const json = await response.json();
+  
+      models.push({ name: json.data[0].id, url: url });
+    }
+  } catch (error) {
+    console.log(error);
   }
-
   return models;
 }
 
@@ -29,19 +31,24 @@ export async function modelOutput(
   maxTokens: number,
   temparature: number,
 ): Promise<string> {
-  const response = await fetch(`${model.url}/v1/completions`, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({
-      model: model.name,
-      prompt: prompt,
-      max_tokens: maxTokens,
-      temparature: temparature,
-    }),
-  });
-  const json = await response.json();
-
-  return json.choices[0].text;
+  let result = "";
+  try {
+    const response = await fetch(`${model.url}/v1/completions`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        model: model.name,
+        prompt: prompt,
+        max_tokens: maxTokens,
+        temparature: temparature,
+      }),
+    });
+    const json = await response.json();
+    result = json.choices[0].text;
+  } catch (error) {
+    console.log(error);
+  } 
+  return result;
 }
